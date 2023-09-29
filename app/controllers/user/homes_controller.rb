e5cda670fd766b0a68245109b3ad7ee3f4c09b32
RyosukeKudo <135448649+Ryo0203VB@users.noreply.github.com>
2023-09-29 10:04:26 +0000

    コミット

--- app/controllers/user/homes_controller.rb
+++ app/controllers/user/homes_controller.rb
@@ -1,17 +1,9 @@
 class User::HomesController < ApplicationController
 
   def top
-    # @posts = Post.all
-    # # 新着商品（４つ）
-    # @posts_latest4 = @posts.first(4)
-    # @genres = Genre.all
   end
-
 private
 
   def set_item
-    # PATHパラメータでitemを取得
-    # @Post = Post.find_by(params[:post_id])
   end
-
 end
